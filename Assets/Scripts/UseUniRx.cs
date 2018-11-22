6ba15851a7d1d2fbf7ddd704b3c15dc8433525ef
hezhangqiang1 <1025440064@qq.com>
2018-11-22 21:45:13 +0800

    基本结构

---
+++ Assets/Scripts/UseUniRx.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+
+public class UseUniRx : MonoBehaviour {
+
+	
+	void Start () {
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.A))
+            .Subscribe(_ =>
+            { Debug.Log("A键按下"); }
+            );
+	}
+	
+	
+	void Update () {
+		
+	}
+}
